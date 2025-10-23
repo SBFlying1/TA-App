e087211dbee63b5098ecf60895761571ece8571f
SBFlying1 <flynnfuller30@gmail.com>
2025-10-23 10:25:48 -0600

    Format Changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    'crispy_forms',
-    'crispy_bootstrap5',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
